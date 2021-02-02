85210df4562e10a66d6e02cdb1f140225474a29f
Owen Beckles <owenbeckles@Owens-MacBook-Pro.local>
2021-02-02 15:49:35 -0500

    Up to Phase 2A

---
+++ myProfile.js
@@ -0,0 +1,7 @@
+window.addEventListener("DOMContentLoaded", event => {
+   let h1 = document.createElement("h1");
+    h1.setAttribute("id", "h1");
+    let name = document.createTextNode("owen stetzer");
+    h1.appendChild(name);
+    body.appendChild(h1);
+});
